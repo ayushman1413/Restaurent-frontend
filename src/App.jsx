fe336176960ba876e1acd4a175769ceeada2471f
Ayushman <ayushman1413@gmail.com>
2025-05-22 13:11:46 +0530

    frontend update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css"
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import Home from './Pages/Home'
+import NotFound from './Pages/NotFound'
+import Success from './Pages/Success'
+
+
+
 const App = () => {
   return <Router>
     <Routes>
-      <Route path="/" element={} />
-      <Route path="/about" element={} />
-      <Route path="/contact" element={} />
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<Success/>} />
+      <Route path="/contact" element={<NotFound/>} />
     </Routes>
   </Router>
 }
