5de41c76691dadb6b5ed0c969d4bfaf8ed52c0da
Ayushman <ayushman1413@gmail.com>
2025-05-22 14:59:51 +0530

    herosection update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
 
 function HeroSection() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="heroSection" id="heroSection">
+      <div className="container">
+        <div className="banner">
+          <div className="largeBox">
+            <h1 className="title">Delicious</h1>
+          </div>
+          <div className="combined_boxes">
+            <div className="imageBox">
+              <img src="/hero.png" alt="hero1" />
+            </div>
+            <div className="textAndLogo">
+              <div className="textWithSvg">
+                <h1 className="title">Food</h1>
+                <h1 className="title dishes_title">Dishes</h1>
+                <img src="/threelines.svg" alt="three" />
+              </div>
+              <img src="/logio.svg" alt="logo"  className="logo"/>
+            </div>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="imageBox">
+            <img src="/hero" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
